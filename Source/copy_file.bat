d4242ede77360ad462b9a22039106457ede2c789
AiHuaLin <kahle.ahl@qq.com>
2024-11-09 15:15:07 +0800

    -

---
+++ Source/copy_file.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set S_PATH=.\config
+set D_PATH=..\bin
+
+pushd "%S_PATH%" 2>nul && (
+    set "S_PATH=%cd%"
+    popd
+)
+
+
+set CONF_PATH=%S_PATH%\config
+set RES_PATH=%S_PATH%\resource
+
+for /R "%D_PATH%" %%f in (xpf.exe) do (
+    if exist "%%f" (
+        echo copy files from "%CONF_PATH%" to "%%~dpf\xpfonfig" ....
+        xcopy "%CONF_PATH%" "%%~dpf\xpfconfig" /E /H /C /I /Y
+        echo copy complete
+    )
+)
+
+for /R "%D_PATH%" %%f in (xpf.exe) do (
+    if exist "%%f" (
+        echo copy files from "%RES_PATH%" to "%%~dpf\xpfresource" ....
+        xcopy "%RES_PATH%" "%%~dpf\xpfresource" /E /H /C /I /Y
+        echo copy complete
+    )
+)
+
+pause
+
+
+
+
--- Source/project.pro
+++ Source/project.pro
@@ -10,12 +10,6 @@ SUBDIRS += \
     XPF \
     Plugins
 
-BEFORE_LINK_CMD_LINE = echo Hello world!
-    !build_pass:system(xcopy /D /F /E /Y \"$${PWD}/Config/XPF.xml\" \"$${PRODIR_BIN}/XPFConfig/XPF.xml\")
-win32 {
-}
-
-
 DISTFILES += \
     $$PRODIR_SRC/Config/XPF.xml \
     $$PRODIR_SRC/Config/XPFPlugins.xml \
